2587d9ac136a91e468274ac73ce2df1bb9a79164
Wanjiru Murira <wanjirumurirah@gmail.com>
2023-04-15 13:43:44 +0300

    updating like route

--- app/routers/post.py
+++ app/routers/post.py
@@ -14,11 +14,9 @@ router= APIRouter(
 @router.get("/", response_model = List[schemas.LikeOut])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),
         limit: int = 10, skip: int = 0, search: Optional[str] = ""):
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
-    results = db.query(models.Post, func.count(models.Like.post_id).label("Likes")).join(models.Like, models.Like.post_id == models.Post.id, 
+    posts = db.query(models.Post, func.count(models.Like.post_id).label("Likes")).join(models.Like, models.Like.post_id == models.Post.id, 
     isouter=True).group_by(models.Post.id).all()
-    return results
-
+    return posts
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model = schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), ):
     #new_post = models.Post(title=post.title, content=post.content, published=post.published)
@@ -28,9 +26,10 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
     db.refresh((new_post))
     return new_post
 
-@router.get("/{id}", response_model = schemas.Post)
+@router.get("/{id}", response_model = schemas.LikeOut)
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    post = posts = db.query(models.Post, func.count(models.Like.post_id).label("Likes")).join(models.Like, models.Like.post_id == models.Post.id, 
+            isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException (status_code=status.HTTP_404_NOT_FOUND, 
                              detail=f"post with id: {id} was not found")
