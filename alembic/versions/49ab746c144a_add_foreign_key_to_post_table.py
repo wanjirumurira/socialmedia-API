ad9a2f4da89817b7f5f5a7248f3b1e2a397d5ae8
MONICAH WANJIRU MURIRA <T34901705@kra.gov>
2023-04-17 14:45:26 +0300

    alembic migration tool

---
+++ alembic/versions/49ab746c144a_add_foreign_key_to_post_table.py
@@ -0,0 +1,24 @@
+"""add foreign key to post_table
+
+Revision ID: 49ab746c144a
+Revises: 41fea6fff0d6
+Create Date: 2023-04-17 12:12:48.446365
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '49ab746c144a'
+down_revision = '41fea6fff0d6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
