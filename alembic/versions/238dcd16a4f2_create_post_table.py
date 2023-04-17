a53ace9e8905b01f21fbaa4fcee6f88497cd0f0e
MONICAH WANJIRU MURIRA <T34901705@kra.gov>
2023-04-17 12:02:01 +0300

    alembic migration tool

---
+++ alembic/versions/238dcd16a4f2_create_post_table.py
@@ -0,0 +1,30 @@
+"""create post table
+
+Revision ID: 238dcd16a4f2
+Revises: 
+Create Date: 2023-04-17 11:07:59.095692
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '238dcd16a4f2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table('posts',sa.Column('id', sa.Integer(), nullable = False, primary_key = True),
+                    sa.Column('title', sa.String(), nullable = False),
+                    sa.Column('content', sa.String(), nullable = False),
+                    )
+
+    pass
+
+
+def downgrade() -> None:
+    op.drop_table('posts')
+    pass
---
+++ alembic/versions/41fea6fff0d6_users_table.py
@@ -0,0 +1,24 @@
+"""users table
+
+Revision ID: 41fea6fff0d6
+Revises: 238dcd16a4f2
+Create Date: 2023-04-17 11:52:37.553355
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '41fea6fff0d6'
+down_revision = '238dcd16a4f2'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
