747cec87a2a1f5165674a8b3d259842e88f2cfca
MONICAH WANJIRU MURIRA <T34901705@kra.gov>
2023-04-18 12:22:09 +0300

    alembic setup

--- alembic/versions/41fea6fff0d6_users_table.py
+++ alembic/versions/41fea6fff0d6_users_table.py
@@ -17,8 +17,17 @@ depends_on = None
 
 
 def upgrade() -> None:
+    op.create_table('users',
+                    sa.Column('id', sa.Integer(), nullable=False),
+                    sa.Column('email', sa.String(), nullable=False),
+                    sa.Column('password', sa.String(), nullable=False),
+                    sa.Column('created_at', sa.TIMESTAMP(timezone=True), nullable=False, server_default=sa.text('now()')),
+                    sa.PrimaryKeyConstraint('id'),
+                    sa.UniqueConstraint('email')
+    )
     pass
 
 
 def downgrade() -> None:
+    op.drop_table('users')
     pass
--- app/main.py
+++ app/main.py
@@ -3,10 +3,14 @@ from . import models
 from .database import engine
 from .routers import post, user, auth, likes
 
-models.Base.metadata.create_all(bind=engine)
+#models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message" : "Hello World"}
+    
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
